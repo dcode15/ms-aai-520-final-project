59bc4196a738842097dbf78aa888f6020fd2e00a
Douglas Code <dcode10@gmail.com>
2024-09-18 17:57:20 -0400

    Sped up fine tuning.

--- src/fine_tune_model.py
+++ src/fine_tune_model.py
@@ -1,3 +1,4 @@
+import torch
 from peft import LoraConfig, get_peft_model
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from trl.trainer import SFTTrainer, SFTConfig
@@ -9,15 +10,15 @@ dataset = Preprocessor.prepare_dataset()
 
 train_test = dataset.train_test_split(test_size=0.2, seed=1)
 
-model = AutoModelForCausalLM.from_pretrained(config.MODEL_NAME)
+model = AutoModelForCausalLM.from_pretrained(config.MODEL_NAME, torch_dtype=torch.float16)
 tokenizer = AutoTokenizer.from_pretrained(config.MODEL_NAME)
 
-model = get_peft_model(model, LoraConfig(**config.LORA_ARGS))
+model = get_peft_model(model, LoraConfig(**config.TUNING_LORA_ARGS))
 
 trainer = SFTTrainer(
     model=model,
     tokenizer=tokenizer,
-    args=SFTConfig(**config.TRAINER_ARGS),
+    args=SFTConfig(**config.TUNING_TRAINER_ARGS),
     train_dataset=train_test['train'],
     eval_dataset=train_test['test']
 )
